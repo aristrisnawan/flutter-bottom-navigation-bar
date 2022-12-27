b1544e7776a8bb1472084c9509b4a99e953ed2c4
aristrisnawan <aristrisnawan35@gmail.com>
2022-12-27 16:58:55 +0700

    add background and active color

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,8 +13,17 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: GNav(
+        tabMargin:EdgeInsets.only(left: 3, right: 3, bottom: 3),
         color: Colors.grey[800],
         duration: Duration(milliseconds: 900),
+        // backgroundColor: Colors.blue,
+        activeColor: Colors.white,
+        tabBackgroundColor: Colors.blue,
+        tabBackgroundGradient: LinearGradient(colors: [Colors.blue, Colors.white], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        tabBorderRadius: 15,
+        onTabChange: (value) {
+          print(value);
+        },
         gap: 8,
         tabs: [
           GButton(icon: Icons.home, text: "Home",),
